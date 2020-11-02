faf3b67b80fd3ab7dae93956f5bdb009c2604363
micki <github@com-science.de>
2020-11-02 22:36:20 +0100

    Further implement Golang implementation of graph

--- golang/graphs/graphs.go
+++ golang/graphs/graphs.go
@@ -1,13 +1,34 @@
 package graphs
 
+import "fmt"
+
 type Node struct {
 	data       interface{}
-	neighbours []Node
+	neighbours []*Node
+	visited    bool
 }
 
 func NewNode(data interface{}) *Node {
 	return &Node{
 		data:       data,
-		neighbours: []Node{},
+		neighbours: []*Node{},
 	}
+}
+
+func (node *Node) Connect(neighbour *Node) {
+	node.neighbours = append(node.neighbours, neighbour)
+}
+
+func (node *Node) String() string {
+	if node.visited {
+		return ""
+	}
+	var s string
+	node.visited = true
+	s += fmt.Sprintf("%v\n", node.data)
+	for _, n := range node.neighbours {
+		s += fmt.Sprintf("%v -- %v\n", node.data, n.data)
+		s += fmt.Sprintf("%v", n)
+	}
+	return s
 }
--- golang/main.go
+++ golang/main.go
@@ -1,9 +1,26 @@
 package main
 
-import "fmt"
-import "github.com/MickiFoerster/graphs/golang/graphs"
+import (
+	"fmt"
+
+	"github.com/MickiFoerster/graphs/golang/graphs"
+)
 
 func main() {
 	A := graphs.NewNode("A")
-    fmt.Println(&A)
+	B := graphs.NewNode("B")
+	C := graphs.NewNode("C")
+	D := graphs.NewNode("D")
+	E := graphs.NewNode("E")
+	F := graphs.NewNode("F")
+	A.Connect(B)
+	A.Connect(C)
+	C.Connect(D)
+	D.Connect(E)
+	D.Connect(F)
+	F.Connect(A)
+
+	fmt.Println("graph {")
+	fmt.Printf("%v\n", A)
+	fmt.Println("}")
 }
