1451e2bc5a799aebf85d8a30c8205557f2da3254
micki <github@com-science.de>
2020-11-07 21:41:10 +0100

    Solution for Rust

--- Rust/main.rs
+++ Rust/main.rs
@@ -1,31 +1,58 @@
+use std::fmt;
+
 #[derive(Debug)]
-struct Node<'z,T> {
+struct Node<T> {
     data: T,
 }
 
-impl<'z,T> Node<'z,T> {
-    fn connect(&mut self, node: &'z Node<'z,T>) {
-        self.neighbours.push(node);
+impl<T: fmt::Display> fmt::Display for Node<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.data)
     }
 }
 
+#[derive(Debug)]
 struct Edge<'z,T> {
-    from: &Node<'z,T>,
-    to  : &Node<'z,T>
+    from: &'z Node<T>,
+    to  : &'z Node<T>
+}
+
+#[derive(Debug)]
+struct Graph<'z, T> {
+    edges: Vec<Edge<'z,T>>
+}
+
+impl<'z, T: std::fmt::Display> Graph<'z, T> {
+    fn connect(&mut self, node_from: &'z Node<T>, node_to: &'z Node<T>) {
+        self.edges.push( Edge::<'z, T>{
+            from: node_from,
+            to: node_to,
+        });
+    }
+
+    fn dot_dump(&self) {
+        println!("graph {{");
+        for e in &self.edges {
+            println!("{} -- {}", e.from, e.to);
+        }
+        println!("}}");
+    }
 }
 
 fn main() {
-   let mut a = Node::<String>{ data: String::from("A"), neighbours: Vec::new() }; 
-   let b = Node::<String>{ data: String::from("B"), neighbours: Vec::new() }; 
-   let mut c = Node::<String>{ data: String::from("C"), neighbours: Vec::new() }; 
-   let mut d = Node::<String>{ data: String::from("D"), neighbours: Vec::new() }; 
-   let e = Node::<String>{ data: String::from("E"), neighbours: Vec::new() }; 
-   let mut f = Node::<String>{ data: String::from("F"), neighbours: Vec::new() }; 
-   a.connect(&b);
-   a.connect(&c);
-   c.connect(&mut d);
-   d.connect(&e);
-   d.connect(&f);
-   f.connect(&a);
-   println!("{:?}", a);
+   let a = Node::<String>{ data: String::from("A") }; 
+   let b = Node::<String>{ data: String::from("B") }; 
+   let c = Node::<String>{ data: String::from("C") }; 
+   let d = Node::<String>{ data: String::from("D") }; 
+   let e = Node::<String>{ data: String::from("E") }; 
+   let f = Node::<String>{ data: String::from("F") }; 
+
+   let mut graph = Graph::<String> { edges: Vec::new() };
+   graph.connect(&a, &b);
+   graph.connect(&a, &c);
+   graph.connect(&c, &d);
+   graph.connect(&d, &e);
+   graph.connect(&d, &f);
+   graph.connect(&f, &a);
+   graph.dot_dump();
 }
